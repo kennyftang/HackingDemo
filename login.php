236d75bb7c806eeef4f59f6da8c2bce3f09a9a0a
Kenny Tang <kennyftang@gmail.com>
2017-03-08 01:35:43 -0600

    Added php file

---
+++ login.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>Super Secure Login</title>
+</head>
+<body>
+<h1>Login Page</h1>
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="login.php">Login</a></li>
+</ul>
+<form method="get">
+    <label>Username:<input name="username" type="text"></label><br/>
+    <label>Password:<input name="username" type="password"></label><br/>
+    <button>Submit</button>
+</form>
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kenny
+ * Date: 07/03/2017
+ * Time: 23:06:43
+ */
